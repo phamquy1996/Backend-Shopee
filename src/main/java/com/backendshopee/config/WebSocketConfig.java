84df381706460bd0a8acf372b6c5b52f9ed230cc
Quy <phamqucntta11@gmail.com>
2021-02-01 00:25:36 +0700

    update

--- src/main/java/com/backendshopee/config/WebSocketConfig.java
+++ src/main/java/com/backendshopee/config/WebSocketConfig.java
@@ -18,11 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/topic");
 		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic");
 	}
 
-	
 }
-
 
--- src/main/java/com/backendshopee/controller/SocketController.java
+++ src/main/java/com/backendshopee/controller/SocketController.java
@@ -5,23 +5,24 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.backendshopee.entity.MessageEntity;
-@CrossOrigin(origins = "http://localhost:3001")
+
 @Controller
 public class SocketController {
 
 	@MessageMapping("/chat.register")
-	@SendTo("/topic/public")
+	@SendTo("/topic/pubic")
 	public MessageEntity register(@Payload MessageEntity chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		System.out.println("handling send message: ");
 		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
 		return chatMessage;
 	}
 
 	@MessageMapping("/chat.send")
-	@SendTo("/topic/public")
+	@SendTo("/topic/pubic")
 	public MessageEntity sendMessage(@Payload MessageEntity chatMessage) {
+		System.out.println("handling send message: ");
 		return chatMessage;
 	}
 
